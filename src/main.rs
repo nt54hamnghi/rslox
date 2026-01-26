70115691a5d94b3debc4b36be0ee7c781089d36e
nt54hamnghi <hamnghi250699@gmail.com>
2026-01-26 07:51:23 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,12 @@ fn main() {
                 String::new()
             });
 
-            // TODO: Uncomment the code below to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
-            // }
+            if !file_contents.is_empty() {
+                // Placeholder, replace this line when implementing the scanner
+                panic!("Scanner not implemented");
+            } else {
+                println!("EOF  null");
+            }
         }
         _ => {
             eprintln!("Unknown command: {}", command);
