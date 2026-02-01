e81d01d073dccdfd6ffa6d57cb3a4b2f9c2ce793
nt54hamnghi <hamnghi250699@gmail.com>
2026-02-01 10:20:53 -0500

    scanning: assignment & equality operator

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,3 +1,4 @@
+use std::iter::Peekable;
 use std::str::Chars;
 
 use crate::error::Report;
@@ -18,14 +19,14 @@ impl<'src> Scanner<'src> {
     pub fn scan_tokens(&self) -> TokenStream<'src> {
         TokenStream {
             line: 1,
-            chars: self.source.chars(),
+            chars: self.source.chars().peekable(),
             at_end: false,
         }
     }
 }
 
 pub struct TokenStream<'src> {
-    chars: Chars<'src>,
+    chars: Peekable<Chars<'src>>,
     line: u32,
     at_end: bool,
 }
@@ -50,6 +51,14 @@ impl<'src> Iterator for TokenStream<'src> {
                 '+' => Token::new(TokenType::Plus, c, None, self.line),
                 '-' => Token::new(TokenType::Minus, c, None, self.line),
                 ';' => Token::new(TokenType::Semicolon, c, None, self.line),
+                '=' => match self.next_match('=') {
+                    Some(nc) => {
+                        let lexeme = [c, nc].iter().collect::<String>();
+                        Token::new(TokenType::EqualEqual, lexeme, None, self.line)
+                    }
+
+                    None => Token::new(TokenType::Equal, c, None, self.line),
+                },
                 _ => {
                     return Some(Err(Report::error(
                         self.line,
@@ -67,12 +76,92 @@ impl<'src> Iterator for TokenStream<'src> {
     }
 }
 
+impl<'src> TokenStream<'src> {
+    /// Attempts to consume the next character if it matches the expected character.
+    ///
+    /// This method peeks at the next character in the iterator. If it matches the `expected`
+    /// character, the character is consumed and returned. Otherwise, `None` is returned and
+    /// the iterator position remains unchanged.
+    fn next_match(&mut self, expected: char) -> Option<char> {
+        let value = self.chars.peek()?;
+        if *value == expected {
+            return self.chars.next();
+        }
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::rstest;
 
     #[rstest]
+    #[case("=", vec![
+        "EQUAL = null",
+        "EOF  null",
+    ])]
+    #[case("==", vec![
+        "EQUAL_EQUAL == null",
+        "EOF  null",
+    ])]
+    #[case("({=}){==}", vec![
+        "LEFT_PAREN ( null",
+        "LEFT_BRACE { null",
+        "EQUAL = null",
+        "RIGHT_BRACE } null",
+        "RIGHT_PAREN ) null",
+        "LEFT_BRACE { null",
+        "EQUAL_EQUAL == null",
+        "RIGHT_BRACE } null",
+        "EOF  null",
+    ])]
+    #[case("((#$%===))", vec![
+        "LEFT_PAREN ( null",
+        "LEFT_PAREN ( null",
+        "[line 1] Error: Unexpected character: #",
+        "[line 1] Error: Unexpected character: $",
+        "[line 1] Error: Unexpected character: %",
+        "EQUAL_EQUAL == null",
+        "EQUAL = null",
+        "RIGHT_PAREN ) null",
+        "RIGHT_PAREN ) null",
+        "EOF  null",
+    ])]
+    #[case("@", vec![
+        "[line 1] Error: Unexpected character: @",
+        "EOF  null",
+    ])]
+    #[case(",.$(#", vec![
+        "COMMA , null",
+        "DOT . null",
+        "[line 1] Error: Unexpected character: $",
+        "LEFT_PAREN ( null",
+        "[line 1] Error: Unexpected character: #",
+        "EOF  null",
+    ])]
+    #[case("%#$@@", vec![
+        "[line 1] Error: Unexpected character: %",
+        "[line 1] Error: Unexpected character: #",
+        "[line 1] Error: Unexpected character: $",
+        "[line 1] Error: Unexpected character: @",
+        "[line 1] Error: Unexpected character: @",
+        "EOF  null",
+    ])]
+    #[case("{(.#;*@+%)}", vec![
+        "LEFT_BRACE { null",
+        "LEFT_PAREN ( null",
+        "DOT . null",
+        "[line 1] Error: Unexpected character: #",
+        "SEMICOLON ; null",
+        "STAR * null",
+        "[line 1] Error: Unexpected character: @",
+        "PLUS + null",
+        "[line 1] Error: Unexpected character: %",
+        "RIGHT_PAREN ) null",
+        "RIGHT_BRACE } null",
+        "EOF  null",
+    ])]
     #[case("(", vec![
         "LEFT_PAREN ( null",
         "EOF  null",
@@ -171,51 +260,6 @@ mod tests {
         "EOF  null",
     ])]
     fn test_scanner(#[case] input: &str, #[case] expected_output: Vec<&str>) {
-        let scanner = Scanner::new(input);
-        let output = scanner
-            .scan_tokens()
-            .map(|t| t.unwrap().to_string())
-            .collect::<Vec<_>>();
-
-        assert_eq!(output, expected_output);
-    }
-
-    #[rstest]
-    #[case("@", vec![
-        "[line 1] Error: Unexpected character: @",
-        "EOF  null",
-    ])]
-    #[case(",.$(#", vec![
-        "COMMA , null",
-        "DOT . null",
-        "[line 1] Error: Unexpected character: $",
-        "LEFT_PAREN ( null",
-        "[line 1] Error: Unexpected character: #",
-        "EOF  null",
-    ])]
-    #[case("%#$@@", vec![
-        "[line 1] Error: Unexpected character: %",
-        "[line 1] Error: Unexpected character: #",
-        "[line 1] Error: Unexpected character: $",
-        "[line 1] Error: Unexpected character: @",
-        "[line 1] Error: Unexpected character: @",
-        "EOF  null",
-    ])]
-    #[case("{(.#;*@+%)}", vec![
-        "LEFT_BRACE { null",
-        "LEFT_PAREN ( null",
-        "DOT . null",
-        "[line 1] Error: Unexpected character: #",
-        "SEMICOLON ; null",
-        "STAR * null",
-        "[line 1] Error: Unexpected character: @",
-        "PLUS + null",
-        "[line 1] Error: Unexpected character: %",
-        "RIGHT_PAREN ) null",
-        "RIGHT_BRACE } null",
-        "EOF  null",
-    ])]
-    fn test_scanner_lexical_errors(#[case] input: &str, #[case] expected_output: Vec<&str>) {
         let scanner = Scanner::new(input);
         let output = scanner
             .scan_tokens()
